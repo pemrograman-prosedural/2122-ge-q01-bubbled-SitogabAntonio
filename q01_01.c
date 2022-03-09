e1ec48bb17670c811161cea41df4f544757082b4
Sitogab Antonio <antonio.girsang@gmail.com>
2022-03-09 15:24:15 +0700

    mencoba persoalan

--- q01_01.c
+++ q01_01.c
@@ -1,11 +1,50 @@
-// NIM - Name
+// 12S21015 - Sitogab Antonio Octavianus Girsang
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argc, char **_argv)
 {
 
-  // codes
+  int j,i,x,len,temp;
+  char str[100],nstr[len-x];
+    x=1;
+    printf("masukkan data : ");
+    gets(str);
+    len = strlen(str);
+    
+    for (i = 0; i < len; i++)
+    {   
+        if (str[i]==','){
+           x++;
+        }
+    }
+    for (i = 0; i < len; i++)
+    {   
+        if (str[i]!=','){
+           nstr[j]=str[i];
+           j++;
+        }
+    }
+    for (i = 0; i < x; i++)
+    {
+        for(j = i + 1; j < x; j++)
+        {
+            if (nstr[i] > nstr[j])
+            {
+                temp = nstr[i];
+                nstr[i] = nstr[j];
+                nstr[j] = temp;
+            }
+        }
+    }
+    for (i = 0; i < x; i++)
+    {
+        printf("%c,", nstr[i]);
+    }
+
 
   return 0;
 }
+
+
